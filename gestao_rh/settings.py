0908adbfbaa19d47c4337c0a578712d5950ca0dd
TassioTaylor <tassiotaylor@gmail.com>
2023-08-01 16:32:58 -0300

    Criando as Apps

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ####
+    'apps.empresa',
+    'apps.funcionarios',
+    'apps.departamentos',
+    'apps.registro_hora_extra',
+    'apps.documentos'
 ]
 
 MIDDLEWARE = [
